d79f02dcf8cd2e21207df0d78ff85915b5ef745a
Xanax <moritz.fuessel@flawcra.cc>
2022-09-20 11:56:00 +0200

    first stuff

---
+++ 01_ITL_Füssel/DrawCharacter.cs
@@ -0,0 +1,49 @@
+﻿namespace DrawCharacter;
+class DrawCharacter
+{
+    static void Main(string[] args)
+    {
+        if (args.Length <= 3)
+        {
+            Console.WriteLine("Ex. usage: DrawCharacter 11 10 k");
+            return;
+        }
+
+        if (!int.TryParse(args[0], out int x))
+        {
+            Console.WriteLine("Invalid Integer: DrawCharacter <-- HERE");
+            return;
+        }
+
+        if (!int.TryParse(args[1], out int y))
+        {
+            Console.WriteLine("Invalid Integer: DrawCharacter " + args[0] + " <-- HERE");
+            return;
+        }
+
+        if (args[2].Length > 1)
+        {
+            Console.WriteLine("Invalid Character length: DrawCharacter " + args[0] + " " + args[1] + " <-- HERE");
+            return;
+        }
+
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.BackgroundColor = ConsoleColor.Gray;
+
+        Console.Clear();
+
+        Console.SetWindowSize(x * 2, y * 2);
+
+        for (int i = 0; i < y; i++)
+        {
+            Console.SetCursorPosition((x / 2), (y / 2) + i);
+
+            for (int j = 0; j < x; j++)
+            {
+                Console.Write(args[2]);
+            }
+        }
+
+        Thread.Sleep(Timeout.Infinite);
+    }
+}
---
+++ SiiSharp/SiiSharp/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace SiiSharp
+{
+    class Program
+    {
+        static void Foo(string[] args)
+        {
+            if(args.Length == 0)
+            {
+                Console.WriteLine("SiiSharp <en/de>");
+                Environment.Exit(0);
+            }
+
+            string test = null;
+            if((test != null) && (test.Length > 0))
+            {
+                Console.WriteLine(test);
+            }
+
+
+
+            switch(args[0])
+            {
+                case "de":
+                    Console.WriteLine("Hallo Welt!");
+                    break;
+                default:
+                    Console.WriteLine("Hello World!");
+                    break;
+
+            }
+
+            string tmpString = args[0] == "en" ? "Hello World" : "Hallo Welt";
+            Console.WriteLine(tmpString);
+        }
+
+        static void TextRepeater(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("SiiSharp <10> \"Hello World\"");
+                Environment.Exit(0);
+            }
+
+            if(!int.TryParse(args[0], out int cnt))
+            {
+                Console.WriteLine("SiiSharp <10> \"Hello World\"");
+                Environment.Exit(0);
+            }
+
+            for(int i = 0; i < cnt; i++)
+            {
+                Console.WriteLine(args[1]);
+            }
+
+            foreach (char item in args[1])
+            {
+                if (item == 'l') continue;
+                if (item == 'o') break;
+                Console.WriteLine(item);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //Foo(args);
+            TextRepeater(args);
+        }
+    }
+}
