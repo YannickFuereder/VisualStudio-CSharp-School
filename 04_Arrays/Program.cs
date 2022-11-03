2dff5bcce6760c1af93104152bcf2b970d306ade
YannickFuereder <yannick.fuereder@gmail.com>
2022-11-03 15:47:17 +0100

    04 arrays initial commit

--- 04_Arrays/Program.cs
+++ 04_Arrays/Program.cs
@@ -1,53 +1,75 @@
 ﻿namespace Arrays;
 
-class Program {
-    static void Array(string[] args) {
-        int[] arr = new int[10];
+class Program
+{
 
-        int[] arr2 = { 1, 3, 4, 4 };
+    static void Main(string[] args)
+    {
+        char? c;
 
-        Console.Write(arr[1]);
+        do
+        {
+            Console.Clear();
+            PrintMenue();
 
-        arr[2] = 10;
+            c = Console.ReadKey().KeyChar;
 
-        for (int i = 0; i < arr2.Length; i++) {
-            Console.Write(arr2[i]);
-        }
+            switch (c)
+            {
+                case '1':
+                    Console.Clear();
+                    HandleMenue1();
+                    break;
+                case '2':
+                    HandleMenue2();
+                    break;
 
-        foreach (int i in arr2) {
-            Console.Write(i);
-        }
+                default:
+                    Console.WriteLine("Unbekannte Auswahl!");
+                    break;
+
+                case '8': break;
+            }
+        } while (c != '8');
     }
 
-    static void Matrix(string[] args) {
-        int[,] matrix = new int[2, 3];
-
-        int[,] matrix2 = { { 120, 125}, { 14, 25 }, { 12, 12 } };
-
-        matrix2[1, 0] = 100;
-
-        for(int row = 0; row < matrix2.GetLength(0); row++) {
-            for (int col = 0; col < matrix2.GetLength(1); col++) {
-                Console.WriteLine(matrix2[row,col]);
-            }
-        }
+    static void PrintMenue()
+    {
+        Console.WriteLine("1. Eindimensionales Array");
+        Console.WriteLine("2. Multipliziere Arrays");
+        Console.WriteLine("3. Primzahlen");
+        Console.WriteLine("4. Zufallszahlen");
+        Console.WriteLine("5. Zweidimensionales Array (gleiche Spaltenlänge)");
+        Console.WriteLine("6. Zweidimensionales Array (unterschiedliche Spaltenlänge)");
+        Console.WriteLine("7. Chess Board");
+        Console.WriteLine("8. Ende");
     }
 
-    static void Main(string[] args) {
-        int[][] matrix = new int[2][];
+    static void HandleMenue1()
+    {
+    }
 
-        matrix[0] = new int[] { 10, 20 };
+    static void HandleMenue2()
+    {
+    }
 
-        matrix[1] = new int[] { 10, 20, 30 };
+    static void HandleMenue3()
+    {
+    }
 
-        Random r = new Random();
+    static void HandleMenue4()
+    {
+    }
 
-        for (int row = 0; row < matrix.Length; row++) {
-            for(int col = 0; col < matrix[row].Length; col++) {
-                matrix[row][col] = r.Next(1000)+1;
-                Console.Write($"{matrix[row][col]}, ");
-            }
-            Console.WriteLine();
-        }
+    static void HandleMenue5()
+    {
+    }
+
+    static void HandleMenue6()
+    {
+    }
+
+    static void HandleMenue7()
+    {
     }
 }
---
+++ 04_Arrays/Util.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+
+namespace Arrays;
+
+internal class Util
+{
+    static void InitArr(int[] arr, int val)
+    {
+        arr = new int[val];
+    }
+
+    static void PrintArr(int[] arr)
+    {
+        foreach (var item in arr)
+        {
+            Console.Write(item + ",");
+        }
+    }
+
+    static int[] MultiplyArr(int[] arr1, int[] arr2)
+    {
+        for (int i = 0; i < arr1.Length; i++)
+        {
+            arr1[i] = arr1[i] * arr2[i];
+        }
+
+        return arr1;
+    }
+
+    static int[] CreatePrimeNumbers(int val)
+    {
+        List<int> primes = new();
+        primes.Add(2);
+        primes.Add(3);
+        while (primes.Count < val)
+        {
+            int nextPrime = (int)(primes[primes.Count - 1]) + 2;
+            while (true)
+            {
+                bool isPrime = true;
+                foreach (int n in primes)
+                {
+                    if (nextPrime % n == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    break;
+                }
+                else
+                {
+                    nextPrime += 2;
+                }
+            }
+            primes.Add(nextPrime);
+        }
+        return primes.ToArray();
+    }
+
+    static int[] CreateRandomNumbers(int count, int maxVal)
+    {
+        Random rand = new();
+
+        int[] arr = new int[count];
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = rand.Next(maxVal);
+        }
+
+        return arr;
+    }
+
+    static void InitTab(int[,] tab, int val)
+    {
+        tab = new int[val,val];
+    }
+
+    static void PrintTab(int[,] tab)
+    {
+        for (int i = 0; i < tab.GetLength(0); i++)
+        {
+            for (int y = 0; y < tab.GetLength(1); y++)
+            {
+                Console.Write(tab[i, y]);
+            }
+
+            Console.WriteLine();
+        }
+    }
+
+    static void InitTab(int[][] tab, int val)
+    {
+        tab = new int[val][];
+    }
+
+    static void PrintTab(int[][] tab)
+    {
+
+    }
+
+    static char[,] CreateChessBoard()
+    {
+
+    }
+
+    static void PrintTab(char[,] tab)
+    {
+
+    }
+}
